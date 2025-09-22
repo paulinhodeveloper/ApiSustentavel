59a0e8243c1fbf29d1b999414d32c76c6b074494
paulinho.borges <paulinhodeveloper@outlook.com>
2025-09-22 19:42:28 -0300

    first commit

---
+++ src/main/java/br/com/sustentavel/api_sustentavel/ApiSustentavelApplication.java
@@ -0,0 +1,13 @@
+package br.com.sustentavel.api_sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSustentavelApplication.class, args);
+	}
+
+}
